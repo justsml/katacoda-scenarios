34dc6432fb20389a7e77421706d49da7ad0b4ef7
Dan Levy <dan@danlevy.net>
2021-04-22 07:10:26 -0600

    update folder structure

--- setup-tracing/foreground.sh
+++ setup-tracing/foreground.sh
@@ -1,2 +1,6 @@
-echo "The IP address for this environment is [[HOST_IP]]"
-echo "The URL to access Port 80 is [[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
+# echo "The IP address for this environment is [[HOST_IP]]"
+# echo "The URL to access Port 80 is [[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
+
+echo "Waiting for Step 1"; while [ ! -f $HOME/node_modules/dd-trace/package.json ] ; 
+  do sleep 2; done;
+echo "Step 1 completed!"
